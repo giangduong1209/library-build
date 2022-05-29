6ba0d056e2570c11a721008ceaf5082aad51e442
giangduong1209 <giangduong1209@gmail.com>
2022-05-29 16:53:59 +0700

    source code

---
+++ component/App.js
@@ -0,0 +1,15 @@
+import html from "../core.js";
+import { connect } from "../store.js";
+
+const connector = connect();
+
+function App({ cars }) {
+  return html`<ul>
+      ${cars.map((car) => `<li>${car}</li>`)}
+    </ul>
+    <button type="button" onclick="dispatch('ADD', 'Porsche')">
+      Add Car
+    </button> `;
+}
+
+export default connector(App);
